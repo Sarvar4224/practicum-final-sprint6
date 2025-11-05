84c96ca0ebc1faf715477af86c08f8a0009a191d
Sarvar Akhmedov <sarvar.akh888@gmail.com>
2025-11-05 22:35:34 +0500

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	srv := server.NewServer(log.Default())
+	err := srv.Server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,61 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("../index.html")
+	if err != nil {
+		http.Error(w, "file not found", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	w.Header().Set("Content-Type", "text/html")
+	if _, err := io.Copy(w, file); err != nil {
+		http.Error(w, "", http.StatusInternalServerError)
+	}
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "no file", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "file read error", http.StatusInternalServerError)
+		return
+	}
+	result, err := service.ConvertMorse(string(data))
+	if err != nil {
+		http.Error(w, "convert error", http.StatusInternalServerError)
+		return
+	}
+
+	newName := time.Now().UTC().Format("20060102") + filepath.Ext(handler.Filename)
+	out, err := os.Create(newName)
+	if err != nil {
+		http.Error(w, "can't create file", http.StatusInternalServerError)
+		return
+	}
+	defer out.Close()
+
+	if _, err := out.Write([]byte(result)); err != nil {
+		http.Error(w, "file write error", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(result))
+}
